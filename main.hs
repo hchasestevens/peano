b42955898ea9bd4018a9f719d6b227ca0c9e747b
H. Chase Stevens <chase@chasestevens.com>
2014-11-25 21:58:19 +0000

    main.hs - add exponentiation

--- main.hs
+++ main.hs
@@ -2,16 +2,19 @@ data Number = Zero | SuccessorOf Number
 	deriving (Show, Eq)
 
 
+-- Addition:
 plus :: Number -> Number -> Number
 x `plus` Zero = x
 x `plus` (SuccessorOf y) = SuccessorOf (x `plus` y)
 
 
+-- Multiplication:
 times :: Number -> Number -> Number
 _ `times` Zero = Zero
 x `times` (SuccessorOf y) = x `plus` (x `times` y)
 
 
+-- Subtraction:
 minus :: Number -> Number -> Number
 x `minus` Zero = x
 (SuccessorOf x) `minus` (SuccessorOf y) = x `minus` y
@@ -50,6 +53,12 @@ x `dividedBy` y = (SuccessorOf nextXDivisionResult, Remainder remainder)
 		divisibleX = x `minus` remainder
 		nextX = divisibleX `minus` y
 		(nextXDivisionResult, _) = (nextX `dividedBy` y)
+
+
+-- Exponentiation
+toThePowerOf :: Number -> Number -> Number
+_ `toThePowerOf` Zero = SuccessorOf Zero
+x `toThePowerOf` (SuccessorOf y) = x `times` (x `toThePowerOf` y)
 		
 
 -- Main:
@@ -89,3 +98,10 @@ main = do
 	let five = six `minus` one
 	putStrLn "6 / 5 ="
 	print $ six `dividedBy` five
+
+	-- Exponentiation
+	putStrLn "3 ** 2 ="
+	print $ three `toThePowerOf` two
+	let nine = three `times` three
+	putStrLn "3 ** 2 == 9 ?"
+	print $ three `toThePowerOf` two `equals` nine
