1ba7b248516a97b02870e7cdaf531059d3c7ad2b
H. Chase Stevens <chase@chasestevens.com>
2014-11-25 21:48:55 +0000

    Add main.hs

---
+++ main.hs
@@ -0,0 +1,91 @@
+data Number = Zero | SuccessorOf Number 
+	deriving (Show, Eq)
+
+
+plus :: Number -> Number -> Number
+x `plus` Zero = x
+x `plus` (SuccessorOf y) = SuccessorOf (x `plus` y)
+
+
+times :: Number -> Number -> Number
+_ `times` Zero = Zero
+x `times` (SuccessorOf y) = x `plus` (x `times` y)
+
+
+minus :: Number -> Number -> Number
+x `minus` Zero = x
+(SuccessorOf x) `minus` (SuccessorOf y) = x `minus` y
+
+
+-- Comparison:
+greaterThan :: Number -> Number -> Number
+(SuccessorOf _) `greaterThan` Zero = (SuccessorOf Zero)
+Zero `greaterThan` _ = Zero
+(SuccessorOf x) `greaterThan` (SuccessorOf y) = x `greaterThan` y
+
+equals :: Number -> Number -> Number
+Zero `equals` Zero = SuccessorOf Zero
+Zero `equals` _ = Zero
+_ `equals` Zero = Zero
+(SuccessorOf x) `equals` (SuccessorOf y) = x `equals` y
+
+
+-- Division:
+modulo :: Number -> Number -> Number
+x `modulo` y 
+	| SuccessorOf _ <- x `equals` y = Zero
+	| Zero <- x `greaterThan` y = x
+	| otherwise = (x `minus` y) `modulo` y
+
+data RemainderNumber = Remainder Number
+	deriving (Show)
+
+type DivisionResult = (Number, RemainderNumber)
+
+dividedBy :: Number -> Number -> DivisionResult
+Zero `dividedBy` x = (Zero, Remainder Zero)
+x `dividedBy` y = (SuccessorOf nextXDivisionResult, Remainder remainder)
+	where
+		remainder = x `modulo` y
+		divisibleX = x `minus` remainder
+		nextX = divisibleX `minus` y
+		(nextXDivisionResult, _) = (nextX `dividedBy` y)
+		
+
+-- Main:
+main :: IO ()
+main = do
+	-- Addition:
+	let one = SuccessorOf Zero
+	let two = SuccessorOf (SuccessorOf Zero)
+	putStrLn "1 + 1 ="
+	print $ one `plus` one
+
+	-- Multiplication:
+	putStrLn "2 * 2 ="
+	print $ two `times` two
+	
+	-- Subtraction:
+	let three = one `plus` two
+	putStrLn "3 - 1 ="
+	print $ three `minus` one
+	putStrLn "3 - 2 ="
+	print $ three `minus` two
+	putStrLn "3 - 3 ="
+	print $ three `minus` three
+	
+	-- Comparison
+	putStrLn "3 > 2 ?"
+	print $ three `greaterThan` two
+	putStrLn "2 > 3 ?"
+	print $ two `greaterThan` three
+	
+	-- Division
+	let six = three `plus` three
+	putStrLn "6 / 3 ="
+	print $ six `dividedBy` three
+	putStrLn "6 / 2 ="
+	print $ six `dividedBy` two
+	let five = six `minus` one
+	putStrLn "6 / 5 ="
+	print $ six `dividedBy` five
